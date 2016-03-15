c249bd0533119c2aa0c0aeeeb6d8deb0183433c4
otterry53 <twlow88@gmail.com>
2016-03-15 00:37:22 -0400

    commit afterworking on js with mentor

--- js/app.js
+++ js/app.js
@@ -6,18 +6,41 @@
    -the user wins if he picks the right number, otherwise, the user receives feedback about how close
     he is, the counter increasees by one and the user's number is added to the list of guesses*/
 
-//First - declare the functions:
+
+//FIRST - Set the global variables:
+
+var secretNumber = computerPick(1, 100);
+var counter = 0
+
+
+//SECOND - declare the functions:
 
 //function for computer to generate secret number between 1 and 100 (Math.random)
-var secretNumber() {
-    math.floor((math.random() * 100) + 1);
+function computerPick(min, max) {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
 }
 
-//??????function to ask user prompt user to "Make your Guess!" OR IS IT JUST THERE?
+//function to give userfeedback in <div id="feedback">
+function userFeedback(userGuess, secretNumber) {
+    var difference = Math.abs(userGuess - secretNumber);
+    if (difference >= 50) {
+        $('#feedback').text("You are freezing! Try again!");
+    } else if (difference > 30) {
+        $('#feedback').text("You are cold! Try again!");
+    } else if (difference > 20) {
+        $('#feedback').text("You are warm! Try again!");
+    } else if (difference > 10) {
+        $('#feedback').text("You are hot! Try again!");
+    } else if (difference > 0) {
+        $('#feedback').text("You're smokin'! Try again!");
+    } else {
+        $('#feedback').text("You win!!!");
+    }
+}
 
-
-//function to validate (HTML5 required flag already set up????????)
-function validate() {
+//function to validate user's guess
+function validate(userGuess) {
     var userGuessCorrectFlag = true;
 
     while (userGuess.length < 1) {
@@ -28,73 +51,63 @@ function validate() {
         var userGuess = prompt("Please enter a number no larger than 100.")
         userGuessCorrectFlag = false;
     }
-    //when number is a number tried before andcis already in #guesslist
-    while (userGuess === {
-            var userGuess = prompt("You already tried that number!");
-            userGuessCorrectFlag = false;
-        }
-        while (Math.floor(userGuess) != userGuess) {
-            var userGuess = prompt("Your guess was not a number. Set it again.");
-            userGuessCorrectFlag = false;
-        }
+
+    while (Math.floor(userGuess) != userGuess) {
+        var userGuess = prompt("Your guess was not a number. Set it again.");
+        userGuessCorrectFlag = false;
     }
-    //function to determine the difference between the numbers
-    function difference(userGuess, secretNumber) {
-        return Math.abs(userGuess - secretNumber);
+    if (userGuessCorrectFlag == true) {
+        userFeedback(userGuess, secretNumber);
+        counter++;
+        guessCounter(counter);
+        userGuessList(userGuess);
+        $('#userGuess').val('');
     }
-    //named function to give userfeedback in <div id="feedback"> - ice cold (+/-50); cold (31-49); warm (21 to 30); hot (11-20);
-    function userFeedback() {
-        if (difference >= 50) {
-            $('#feedback').prompt("You are freezing! Try again!");
-        } else if (difference > 30) {
-            $('#feedback').prompt("You are cold! Try again!");
-        } else if (difference > 20) {
-            $('#feedback').prompt("You are warm! Try again!");
-        } else if (difference > 10) {
-            $('#feedback').prompt("You are hot! Try again!");
-        } else if (difference > 0) {
-            $('feedback').prompt("You are smokin'! Try again!");
-        } else {
-            $('#feedback').prompt("You win!!!");
-        }
+}
+
+//function to supply user with list of nos. guessed; append as <li> to <ul -id = "guessList">
+function userGuessList(userGuess, secretNumber) {
+    if (userGuess != secretNumber) {
+        $('#guessList').append("<li>" + userGuess + "</li>");
     }
+}
 
-    //function to supply user with list of numbers already guess; append as <li> to <ul -id = "guesslist">
-    function userGuessList() {
-        if (userGuess != secretNumber) {
-            $('#guessList').append("<li>" + userGuess + "</li>");
-        }
-    }
-    //functon to count number of guesses and give feedback in <span id="count">
-    function guessCounter() {
-        if (userGuess != comPick) {
-            $('#count').add(1);
-        }
-    }
-    //function to cause clicking "New Game" to trigger JavaScript function that starts a new game
-    function startNewGame() {
-        $(".new").click(function () {
-            $('#count').val(0);
-            $('#guessList').empty();
-        });
-    }
+//functon to count number of guesses and give feedback in <span id="count">
+function guessCounter(userGuess) {
+    $('#count').text(counter);
+}
 
 
-    //Then - Use the functions
+//function to cause clicking "New Game" to trigger JavaScript function that starts a new game
+function startNewGame() {
+    //document.location.reload(true);
+    $(".new").click(function () {
+        $('#count').text(0);
+        counter = 0;
+        $('#guessList').empty();
+        $('#feedback').text("Make your Guess!")
+    });
+}
 
-    $(document).ready(function () {
+//THIRD Use the functions
+$(document).ready(function () {
 
-        /*--- Display information modal box ---*/
-        $(".what").click(function () {
-            $(".overlay").fadeIn(1000);
+    $('.new').on('click', startNewGame);
 
-        });
-        /*--- Hide information modal box ---*/
-        $("a.close").click(function () {
-            $(".overlay").fadeOut(1000);
-        });
+    $('#guessButton').on('click', function () {
+        //first get the value that user added in the input box
+        var userGuess = $('#userGuess').val();
+        var newGuess = userGuess;
+        validate(userGuess);
+    });
 
-        startNewGame(secretNumber);
-        var userGuess = prompt("Make your Guess.");
+    /*--- Display information modal box ---*/
+    $(".what").click(function () {
+        $(".overlay").fadeIn(1000);
 
     });
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function () {
+        $(".overlay").fadeOut(1000);
+    });
+});
